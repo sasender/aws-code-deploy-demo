b6650674d2b041b357a33e5403a05cd0918ea536
niccololampa <niccololampa@gmail.com>
2021-06-12 16:11:11 +0800

    Update comments on app-stop.sh

--- aws-scripts/app-stop.sh
+++ aws-scripts/app-stop.sh
@@ -1,1 +1,4 @@
+#Run commands here to gracefull stop the application
+# Can also be used to remvoe currently installed packages in preparation for deployment
+# Does not run upon your first deploy.
 #!/bin/bash
