4c9f3d692bb97366f55a4b13bed0aad3f99cf317
niccololampa <niccololampa@gmail.com>
2021-06-12 14:32:25 +0800

    Update aws scripts for auto deploy

---
+++ aws-scripts/after-install.sh
@@ -0,0 +1,4 @@
+# insert commands essential after copying the project repo via code deploy in this file
+# For example downloading files from other resources to be placed in the repo folder
+
+#!/bin/bash
