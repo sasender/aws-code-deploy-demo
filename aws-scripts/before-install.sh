94f3282433716fddbe1ceccf99ed39aa0757f2a3
sasender <78895362+sasender@users.noreply.github.com>
2022-01-05 11:08:02 +0530

    Update before-install.sh

--- aws-scripts/before-install.sh
+++ aws-scripts/before-install.sh
@@ -3,12 +3,12 @@
 
 #install docker
 sudo apt update
-yes | sudo apt install apt-transport-https ca-certificates curl software-properties-common
+yes | sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu  $(lsb_release -cs)  stable"
 sudo apt update
 apt-cache policy docker-ce
-yes | sudo apt install docker-ce
+yes | sudo apt-get install docker-ce
 
 # update user to not require sudo for docker commands
 sudo usermod -aG docker ${USER}
